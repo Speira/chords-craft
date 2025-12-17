c52407d1c95106019259a5062e69069460a89e71
Speira <jl.robert.91@gmail.com>
2025-12-17 01:34:27 +0100

    feat: complete chart lambda

--- packages/apps/api/chart/src/index.ts
+++ packages/apps/api/chart/src/index.ts
@@ -1,39 +1,19 @@
-
-import { Effect } from "effect";
-
 import type { AppSyncResolverEvent } from "aws-lambda";
 
-import {
-  ChartServicesLive,
-  CreateChartCommand,
-  CreateChartHandler,
-  GetChartHandler,
-  GetChartQuery,
-  ListChartHandler,
-  ListChartQuery,
-} from "@speira/chordschart-contexts-chart";
+import { ChartInterface } from "@speira/chordschart-contexts-chart";
 
-export const handler = async (event: AppSyncResolverEvent<any>) => {
+export const handler = async (
+  event: AppSyncResolverEvent<Record<string, unknown>>
+) => {
   const { fieldName } = event.info;
-  const args = event.arguments;
-
-  const program = (() => {
-    switch (fieldName) {
-      case "createChart":
-        return CreateChartHandler.execute(new CreateChartCommand(args));
-      
-      case "getChart":
-        return GetChartHandler.execute(new GetChartQuery(args));
-      
-      case "listCharts":
-        return ListChartHandler.execute(new ListChartQuery(args));
-      
-      default:
-        return Effect.fail(new Error(`Unknown field: ${fieldName}`));
-    }
-  })();
-
-  return Effect.runPromise(
-    program.pipe(Effect.provide(ChartServicesLive))
-  );
+  switch (fieldName) {
+    case "createChart":
+      return ChartInterface.graphql.resolvers.createChart(event);
+    case "getChart":
+      return ChartInterface.graphql.resolvers.getChart(event);
+    case "listCharts":
+      return ChartInterface.graphql.resolvers.listCharts(event);
+    default:
+      throw new Error(`Unknown field: ${fieldName}`);
+  }
 };
