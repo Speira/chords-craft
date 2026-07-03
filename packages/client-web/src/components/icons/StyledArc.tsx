0a13c75aeb8cb02f6acbbc7f9f228d6e064f7515
Speira <jl.robert.91@gmail.com>
2026-07-03 17:29:19 +0200

    fix: fix lint context-chart

--- packages/client-web/src/components/icons/StyledArc.tsx
+++ packages/client-web/src/components/icons/StyledArc.tsx
@@ -2,7 +2,19 @@ import Image from "next/image";
 
 export const StyledArc = () => (
   <>
-    <Image src="/arc.svg" alt="home-header-image" fill objectFit="contain" className="dark:hidden" />
-    <Image src="/arc-dark.svg" alt="home-header-image" fill objectFit="contain" className="hidden dark:block" />
+    <Image
+      src="/arc.svg"
+      alt="home-header-image"
+      fill
+      objectFit="contain"
+      className="dark:hidden"
+    />
+    <Image
+      src="/arc-dark.svg"
+      alt="home-header-image"
+      fill
+      objectFit="contain"
+      className="hidden dark:block"
+    />
   </>
 );
