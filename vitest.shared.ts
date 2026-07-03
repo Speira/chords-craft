936c48e08e75751c7f57ff9286e3e6d53018fa68
Speira <jl.robert.91@gmail.com>
2026-07-03 17:26:01 +0200

    test: fix test for api chart

--- vitest.shared.ts
+++ vitest.shared.ts
@@ -2,4 +2,5 @@ import * as path from "node:path";
 
 export const workspaceAliases = {
   "@speira/chordschart-shared": path.join(__dirname, "packages/shared/src"),
+  "@speira/chordschart-context-chart": path.join(__dirname, "packages/context-chart/src"),
 };
